a5380b48db822ec1109f3253cc27df076be7c745
lagrandecode <seunogunmolufirst1@gmail.com>
2022-07-20 00:35:53 +0100

    "commit"

--- api/serializers.py
+++ api/serializers.py
@@ -1,2 +1,17 @@
+
+from dataclasses import fields
 from django.contrib.auth.models import User, Group
-from rest_framework 
+from rest_framework import serializers
+
+
+class UserSerializers(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+
+class GroupSerializers(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = '__all__'
