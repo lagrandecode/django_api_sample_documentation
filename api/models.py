3e9e5d888efd4a1677c62b2d1890aa4500ccc15c
lagrandecode <seunogunmolufirst1@gmail.com>
2022-07-20 00:35:33 +0100

    "commit"

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
+
 from django.db import models
 
 # Create your models here.
+
+class User(models.model):
+    url = models.URLField()
+    username = models.CharField(max_length=50)
+    email = models.EmailField()
+
+
+class Group(models.Model):
+    url = models.URLField()
+    name = models.CharField(max_length=60)
+
